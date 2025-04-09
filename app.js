1a8bb0bdafd2a408bb9fa726b3fd20153c07a5e6
ismailbaoud <ibaoud46@gmail.com>
2025-04-09 15:49:46 +0100

    Ajout de la gestion des routes et de la gestion des erreurs dans l'application Express.

--- app.js
+++ app.js
@@ -1,23 +1,15 @@
-var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+var createError = require('http-errors');
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
-app.use(logger('dev'));
-app.use(express.json());
+// importer les routes
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -32,10 +24,19 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+  
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  // res.status(err.status || 500);
+  res.json({ message: 'errorcc'});
 });
 
+const port = 3000;
+
+app.listen(port, ()=>{
+  console.log((`Server running on http://localhost:${port}`));
+  
+})
+
 module.exports = app;
+
+
