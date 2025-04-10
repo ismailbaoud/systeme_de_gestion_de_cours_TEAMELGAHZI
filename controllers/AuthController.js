60379bcfbc567ccec546dbc45ba79050ea04c310
ismailbaoud <ibaoud46@gmail.com>
2025-04-10 11:15:29 +0100

    Importer les modules nécessaires pour l'authentification des utilisateurs : UserModel, bcrypt, jwt

---
+++ controllers/AuthController.js
@@ -0,0 +1,13 @@
+const UserModel = require('../models/UserModel');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+
+
+class AuthController {
+
+
+   
+}
+
+module.exports = new AuthController;
