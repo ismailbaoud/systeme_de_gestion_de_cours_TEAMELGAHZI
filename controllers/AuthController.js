f5ce84ae47268d43ff770f8e7b6ae819191deaa4
ismailbaoud <ibaoud46@gmail.com>
2025-04-10 11:17:03 +0100

    ajout de method login avec la generation de token JWT

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -18,7 +18,41 @@ class AuthController {
         }
     }
 
-  
+    async login(req, res) {
+        try {
+          const { email, password } = req.body;
+      
+          const user = await UserModel.findOne({ where: { email } });
+      
+          if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+          }
+      
+          const isPasswordValid = await bcrypt.compare(password, user.password);
+      
+          if (!isPasswordValid) {
+            return res.status(401).json({ message: 'Invalid password' });
+          }
+      
+          const token = jwt.sign(
+            { id: user.id, email: user.email },
+            process.env.JWT_SECRET,
+            { expiresIn: '1d' }
+          );
+          console.log(token);
+          
+      
+           res.status(200).json({ message: 'Login successful' , token});
+      
+        } catch (error) {
+          console.error("Login error:", error);
+          return res.status(500).json({ message: 'Login failed', error });
+        }
+      }
+      
+
+
+ 
 }
 
 module.exports = new AuthController;
