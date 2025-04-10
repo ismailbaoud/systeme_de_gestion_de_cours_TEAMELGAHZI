fd6119188971e69a8e7c5d0fe3b32a8dd173f0f7
ismailbaoud <ibaoud46@gmail.com>
2025-04-10 11:16:12 +0100

    ajout de method register

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -7,7 +7,18 @@ const jwt = require('jsonwebtoken');
 class AuthController {
 
 
-   
+    async register(req, res){
+        try{
+            const {name , email, password} = req.body;
+            const hashPassword = await bcrypt.hash(password, 10);
+            const user = await UserModel.create({name , email ,password: hashPassword});            
+            res.status(201).json('added successfally');
+        }catch{
+
+        }
+    }
+
+  
 }
 
 module.exports = new AuthController;
