6551fe3856fbde6634b5aa94a9b55847d0655d14
ismailbaoud <ibaoud46@gmail.com>
2025-04-11 22:01:06 +0100

    Set up multer for file uploads with unique filenames and storage directory

---
+++ backend/middlewares/upload.js
@@ -0,0 +1,16 @@
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/'); // folder to store videos
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, uniqueSuffix + path.extname(file.originalname)); // eg. 167388491.mp4
+  }
+});
+
+const upload = multer({ storage: storage });
+
+module.exports = upload;
